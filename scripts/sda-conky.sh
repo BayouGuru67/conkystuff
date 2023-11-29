916a665db9940536d7e1270ed7fd2c5a289dbf2d
Bayou Guru <bayouguru67@gmail.com>
2023-11-29 05:46:07 -0700

    Updated config
    Main conky now 6 pixels smaller.  Corrected some minor alignment issues.

--- scripts/sda-conky.sh
+++ scripts/sda-conky.sh
@@ -3,7 +3,7 @@
 smartctla_output=$(/usr/sbin/smartctl -a /dev/sda | grep "overall-health" | awk '{print "\""$NF"\""}')
 # Check if the output is PASSED and display the appropriate image
 if [ "$smartctla_output" == "\"PASSED\"" ]; then
-    echo "\${image /home/bayouguru/.conky/images/green.png -p 4,636 -s 16x16 } "
+    echo "\${image /home/bayouguru/.conky/images/green.png -p 4,628 -s 16x16 } "
 else
-    echo "\${image /home/bayouguru/.conky/images/red.png -p 4,636 -s 16x16 } "
+    echo "\${image /home/bayouguru/.conky/images/red.png -p 4,628 -s 16x16 } "
 fi
