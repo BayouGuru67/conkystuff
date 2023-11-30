e21e0c71f33cef58d42693cf29c20bef9e8b9ccc
Bayou Guru <bayouguru67@gmail.com>
2023-11-30 06:22:26 -0700

    Updated main and right Conkys
    Moved the # of active and the total number of processes from the right conky to the left conky and reduced the size of the top 5 headers to make a bit more room for showing all of the network connections (when approaching the max of 18 displayed) on-screen, including up to 6 inbound and 12 outbound connections.  This also meant a realignment of all of the images except for the capslock and numlock indicators.

--- scripts/sdb-conky.sh
+++ scripts/sdb-conky.sh
@@ -3,7 +3,7 @@
 smartctlb_output=$(/usr/sbin/smartctl -a /dev/sdb | grep "overall-health" | awk '{print "\""$NF"\""}')
 # Check if the output is PASSED and display the appropriate image
 if [ "$smartctlb_output" == "\"PASSED\"" ]; then
-    echo "\${image /home/bayouguru/.conky/images/green.png  -p 4,710 -s 16x16}"
+    echo "\${image /home/bayouguru/.conky/images/green.png  -p 4,744 -s 16x16}"
 else
-    echo "\${image /home/bayouguru/.conky/images/red.png  -p 4,710 -s 16x16}"
+    echo "\${image /home/bayouguru/.conky/images/red.png  -p 4,744 -s 16x16}"
 fi
