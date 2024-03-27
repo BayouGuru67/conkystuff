db6cafe69ffc121fbbd7292d0b755e0e67b630ba
Bayou Guru <bayouguru67@gmail.com>
2024-03-27 07:31:24 -0600

    Delete scripts/sdb-conky.sh

--- scripts/sdb-conky.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# Run the smartctl command and capture the output in a variable
-smartctlb_output=$(/usr/sbin/smartctl -a /dev/sdb | grep "overall-health" | awk '{print "\""$NF"\""}')
-# Check if the output is PASSED and display the appropriate image
-if [ "$smartctlb_output" == "\"PASSED\"" ]; then
-    echo "\${image /home/bayouguru/.conky/images/green.png  -p 4,744 -s 16x16}"
-else
-    echo "\${image /home/bayouguru/.conky/images/red.png  -p 4,744 -s 16x16}"
-fi
