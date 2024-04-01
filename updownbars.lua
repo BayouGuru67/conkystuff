2a1f4647d6826d6f0563b54052174a100a1bfedf
Bayou Guru <bayouguru67@gmail.com>
2024-04-01 06:18:26 -0600

    Changed upload/download bars
    Changed the layout of the upload/download section and changed the right conky's  lua bars to a longer layout in the horizontal position instead of vertical for more responsiveness/detailed metering (Now 55+ segments vs. 12!) and edited some various other parts of the config.

--- updownbars.lua
+++ updownbars.lua
@@ -151,10 +151,10 @@ end
 --[[ equalizer(cr, xb, yb, name, arg, max, nb_blocks, cap, w, h, space, bgc, bga, fgc, fga,alc,ala,alarm,led_effect,led_alpha,smooth,mid_color,mid_alpha,rotation) ]]
 -- Upload
 cr = cairo_create(cs)
-equalizer(cr, 6, 331, 'upspeedf', 'enp6s0', 20000, 10, CAIRO_LINE_CAP_SQUARE, 34, 2, 1, 0x606070, 1, 0x00ff0c, 1, 0xff0000, 1, 80, true, 1, true, 0xffff00, 1, 0)
+equalizer(cr, 128, 297, 'upspeedf', 'enp6s0', 20000, 57, CAIRO_LINE_CAP_SQUARE, 10, 2, 1, 0x606070, 1, 0x00ff0c, 1, 0xff0000, 1, 80, true, 1, true, 0xffff00, 1, 90)
 cairo_destroy(cr)
 -- Download
 cr = cairo_create(cs)
-equalizer(cr, 40, 371, 'downspeedf', 'enp6s0', 100000, 10, CAIRO_LINE_CAP_SQUARE, 34, 2, 1, 0x606070, 1, 0x00ff0c, 1, 0xff0000, 1, 80, true, 1, true, 0xffff00, 1, 180)
+equalizer(cr, 128, 315, 'downspeedf', 'enp6s0', 50000, 57, CAIRO_LINE_CAP_SQUARE, 10, 2, 1, 0x606070, 1, 0x00ff0c, 1, 0xff0000, 1, 80, true, 1, true, 0xffff00, 1, 90)
 cairo_destroy(cr)
 end
