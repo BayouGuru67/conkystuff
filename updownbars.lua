0ae55551b6389f30a6c022f35b93abe2fb536b1a
Bayou Guru <bayouguru67@gmail.com>
2024-05-24 06:43:31 -0600

    Updated positioning
    Updated positioning.

--- updownbars.lua
+++ updownbars.lua
@@ -152,10 +152,10 @@ end
 --[[ equalizer(cr, xb, yb, name, arg, max, nb_blocks, cap, w, h, space, bgc, bga, fgc, fga,alc,ala,alarm,led_effect,led_alpha,smooth,mid_color,mid_alpha,rotation) ]]
 -- Upload
 cr = cairo_create(cs)
-equalizer(cr, 77, 347, 'upspeedf', 'enp6s0', 20000, 74, CAIRO_LINE_CAP_SQUARE, 10, 2, 1, 0x606070, 1, 0x00ff0c, 1, 0xff0000, 1, 80, true, 1, true, 0xffff00, 1, 90)
+equalizer(cr, 77, 343, 'upspeedf', 'enp6s0', 20000, 74, CAIRO_LINE_CAP_SQUARE, 10, 2, 1, 0x606070, 1, 0x00ff0c, 1, 0xff0000, 1, 80, true, 1, true, 0xffff00, 1, 90)
 cairo_destroy(cr)
 -- Download
 cr = cairo_create(cs)
-equalizer(cr, 77, 364, 'downspeedf', 'enp6s0', 50000, 74, CAIRO_LINE_CAP_SQUARE, 10, 2, 1, 0x606070, 1, 0x00ff0c, 1, 0xff0000, 1, 80, true, 1, true, 0xffff00, 1, 90)
+equalizer(cr, 77, 360, 'downspeedf', 'enp6s0', 50000, 74, CAIRO_LINE_CAP_SQUARE, 10, 2, 1, 0x606070, 1, 0x00ff0c, 1, 0xff0000, 1, 80, true, 1, true, 0xffff00, 1, 90)
 cairo_destroy(cr)
 end
